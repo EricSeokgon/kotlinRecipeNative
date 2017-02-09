8d206ad655df8b9521d84077293e59a57fc78140
sklee <hadesleeq@gmail.com>
2017-02-09 17:11:07 +0900

    Initial commit

---
+++ src/Test.kt
@@ -0,0 +1,12 @@
+/**
+ * Project: kotlinRecipe
+ * FileName: Test
+ * Date: 2017-02-09
+ * Time: 오후 5:07
+ * Author: user
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+class Test {
+    var subject = ""
+}
